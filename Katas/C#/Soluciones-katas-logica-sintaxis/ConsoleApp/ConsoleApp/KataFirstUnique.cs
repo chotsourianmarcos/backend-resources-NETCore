3e8780ee4a895cc7dd3a958cf2f5a87e62e1a5e0
USUARIO <chotsourian.marcos@gmail.com>
2023-02-15 13:39:22 +0100

    solucionesC#

---
+++ Katas/C#/Soluciones-katas-logica-sintaxis/ConsoleApp/ConsoleApp/KataFirstUnique.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class KataFirstUnique
+    {
+        public int Solution(int[] A)
+        {
+            int defaultResult = -1;
+            List<int> notRepeatedNums = new List<int>();
+
+            for (var i = 0; i < A.Length; i++)
+            {
+                var counter = 0;
+                for (var j = 0; j < A.Length; j++)
+                {
+                    if (A[i] == A[j])
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 1)
+                {
+                    notRepeatedNums.Add(A[i]);
+                }
+            }
+
+            if (notRepeatedNums.Count > 0)
+            {
+                return notRepeatedNums[0];
+            }
+            else
+            {
+                return defaultResult;
+            }
+        }
+    }
+}
