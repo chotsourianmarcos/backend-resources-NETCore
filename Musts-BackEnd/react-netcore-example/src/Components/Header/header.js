e992fe1befc18d983419139604432966836ce1f1
mchotsourian <mchotsourian@sgfintech.com.ar>
2023-02-20 21:19:33 +0100

    react-netcore-example-v0.1

---
+++ Musts-BackEnd/react-netcore-example/src/Components/Header/header.js
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header() {
+  return (
+    <div id="headerContainer">
+      <h1 id="headerTitle">Sistema Administrativo de Recursos Humanos</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ Musts-BackEnd/react-netcore-example/src/Components/NavBar/NavButton/navButton.js
@@ -0,0 +1,11 @@
+import './navButton.css';
+
+function NavButton() {
+  return (
+    <button>
+        
+    </button>
+  );
+}
+
+export default NavButton;
