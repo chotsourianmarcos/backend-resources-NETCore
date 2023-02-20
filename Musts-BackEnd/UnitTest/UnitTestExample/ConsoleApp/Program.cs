32e72a3cc17209039c9cdf88520dfbc880f84a0f
mchotsourian <mchotsourian@sgfintech.com.ar>
2023-02-20 15:08:21 +0100

    UnitTestExample-rounded-v1.0

--- Musts-BackEnd/UnitTest/UnitTestExample/ConsoleApp/Program.cs
+++ Musts-BackEnd/UnitTest/UnitTestExample/ConsoleApp/Program.cs
@@ -1,7 +1,3 @@
 ﻿using ConsoleApp;
 
-//KataFirstUnique
-var kataFirstUnique = new KataFirstUnique();
-int[] intArrayA = {4, 10, 5, 4, 2, 10};
-var result = kataFirstUnique.Solution(intArrayA);
-Console.WriteLine(result);
+Console.WriteLine("Hola!");
--- Musts-BackEnd/UnitTest/UnitTestExample/UnitTest/KataFirstUniqueTest.cs
+++ Musts-BackEnd/UnitTest/UnitTestExample/UnitTest/KataFirstUniqueTest.cs
@@ -1,5 +1,4 @@
 using ConsoleApp;
-using System.ComponentModel.DataAnnotations;
 
 namespace UnitTest
 {
@@ -10,20 +9,21 @@ namespace UnitTest
         public void TestSolution()
         {
             var kataFirstUnique = new KataFirstUnique();
+
             int[] intArrayA = { 4, 10, 5, 4, 2, 10 };
             var expected = 5;
             var actual = kataFirstUnique.Solution(intArrayA);
-            Assert.AreEqual(expected, actual, 0, "Error message Sasasa");
+            Assert.AreEqual(expected, actual, 0, "Custom error message");
 
             int[] intArrayB = { 4, 10, 5, 5, 3, 4, 2, 10 };
             expected = 3;
             actual = kataFirstUnique.Solution(intArrayB);
-            Assert.AreEqual(expected, actual, 0, "Error message Sasasa");
+            Assert.AreEqual(expected, actual, 0, "Custom error message");
 
             int[] intArrayC = { 4, 10, 5, 4, 2, 10, 5, 2, 7 };
             expected = 7;
             actual = kataFirstUnique.Solution(intArrayC);
-            Assert.AreEqual(expected, actual, 0, "Error message Sasasa");
+            Assert.AreEqual(expected, actual, 0, "Custom error message");
         }
     }
 }
