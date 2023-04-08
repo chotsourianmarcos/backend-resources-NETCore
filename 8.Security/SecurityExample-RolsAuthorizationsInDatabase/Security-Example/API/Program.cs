fc4d1a06d159309b3f734aa4387c45cdd87d961c
mchotsourian <mchotsourian@sgfintech.com.ar>
2023-04-08 11:20:20 +0200

    swagger-authorizations

--- 8.Security/SecurityExample-RolsAuthorizationsInDatabase/Security-Example/API/Program.cs
+++ 8.Security/SecurityExample-RolsAuthorizationsInDatabase/Security-Example/API/Program.cs
@@ -5,6 +5,7 @@ using Data;
 using Logic.ILogic;
 using Logic.Logic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter authorization",
+        Name = "EndpointAuthorize",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "Bearer",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 builder.Services.AddScoped<IUserSecurityService, UserSecurityService>();
 builder.Services.AddScoped<IUserService, UserService>();
